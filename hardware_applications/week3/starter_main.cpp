e00d74ff6090eef2c9f0d2698a63db2c9c10f120
Chandler Moeller <chandlermoeller@gmail.com>
2017-10-12 20:06:09 -0400

    updated starter_main

--- hardware_applications/week3/starter_main.cpp
+++ hardware_applications/week3/starter_main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <STSL/RJRobot.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,7 +9,32 @@ int main() {
 
     RJRobot robot;
 
+    vector<int> measurements;
 
+    int black_threshold = 100;
+
+    int gray_threshold = 85;
+
+    while(measurements.empty() || measurements.back() > black_threshold) {
+        measurements.push_back(robot.LightValue());
+
+        robot.SetMotor(MotorPort::A, 100);
+        robot.SetMotor(MotorPort::B, 100);
+        robot.Wait(250ms);
+
+    }
+
+    cout << measurements.back() << endl;
+
+    robot.StopMotors();
+
+    measurements.erase(measurements.end() - 1);
+
+    auto is_gray = bind2nd(less<int>(), gray_threshold);
+
+    int number_gray = count_if(measurements.begin(), measurements.end(), is_gray);
+
+    cout << number_gray << " grey squares found" << endl;
 
     return 0;
 }
